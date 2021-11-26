a3fbeba86c3ee0a7a6b1d497ec1fd68e4e888399
AwiZy63 <awizy0609@gmail.com>
2021-11-26 14:59:10 +0100

    Ajout de la page Groupe Plastic Duck

--- pages/BandScreen.js
+++ pages/BandScreen.js
@@ -13,7 +13,7 @@ export default function BandScreen() {
                     <Image style={styles.screenHeaderImage} resizeMode={'cover'} source={require('../assets/plasticduck/pc3.jpg')} />
                     <View style={styles.screenHeaderImageCover}></View>
                     <Grid style={styles.screenHeaderBanner}>
-                        <Row style={{position: 'relative'}}>
+                        <Row style={{ position: 'relative' }}>
                             <Col style={{ flex: 1 }}>
                                 <Image resizeMode={'center'} style={styles.screenHeaderBannerLogo} source={require('../assets/plasticduck/logo_big.png')} />
                             </Col>
@@ -25,11 +25,52 @@ export default function BandScreen() {
                     </Grid>
                 </View>
                 <Grid style={styles.formationContainer}>
-                    <Col>
                     <Row><Text style={styles.formationTitle}>Formation</Text></Row>
-                    </Col>
-                    <Col></Col>
+                    <Grid>
+                        <Col style={{ flex: 1 }}>
+                            <Row><Text style={styles.formationRowNameText}>Benoit AMBROZY</Text></Row>
+                            <Row><Text style={styles.formationRowNameText}>Marc SZARCZYNSKI</Text></Row>
+                            <Row><Text style={styles.formationRowNameText}>Julien HATTON</Text></Row>
+                            <Row><Text style={styles.formationRowNameText}>Chris CAMPHIN</Text></Row>
+                            <Row><Text style={styles.formationRowNameText}>Jérôme DUPUICH</Text></Row>
+                            <Row><Text style={styles.formationRowNameText}>Prenom NOM</Text></Row>
+                        </Col>
+                        <Col style={{ flex: 2 }}>
+                            <Row><Text style={styles.formationRowPracticeText}>Chant, saxophones (soprano, alto, ténor, baryton)</Text></Row>
+                            <Row><Text style={styles.formationRowPracticeText}>Chant, guitares (classique, manouche, électrique)</Text></Row>
+                            <Row><Text style={styles.formationRowPracticeText}>Choeur, piano, accordéon</Text></Row>
+                            <Row><Text style={styles.formationRowPracticeText}>Choeur, guitare, guitare basse</Text></Row>
+                            <Row><Text style={styles.formationRowPracticeText}>Batterie, percussions</Text></Row>
+                            <Row><Text style={styles.formationRowPracticeText}>Trombone, guitare basse</Text></Row>
+                        </Col>
+                    </Grid>
                 </Grid>
+                <View style={styles.bioContainer}>
+                    <Text style={styles.formationTitle}>Biographie</Text>
+                    <Text style={styles.bioText}>Après des expériences musicales variées, Benoît et Marc décident de se lancer dans une nouvelle aventure, celle des Plastic Duck.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Tout un univers est alors créé se nourrissant de leurs influences respectives ainsi que de leur goût commun pour la chanson française, autant de leur enfance comme Brel, Brassens, Maxime le Forestier, que de chanteurs plus actuels tels que Sanseverino, Yves Jamait ou Zoufris Maracas.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Dans leur texte, ces deux amis de longue date abordent des sujets actuels et sensibles de façon humoristique voire osée. « L’humour, c’est le droit d’être imprudent, d’avoir le courage de déplaire, la permission absolue d’être imprudent. » (Pierre Desproges - 1986)</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Toujours en quête de partage, ils décidèrent d’intégrer de nouveaux comédiens à la formation initiale.
+                        Ces personnages, partageant les mêmes ambitions, rejoignirent le groupe en apportant de nouvelles couleurs et des sonorités plus swing.
+                        Le premier spectacle est alors monté pour une première représentation en 2015.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Après quelques concerts dans des salles locales, des festivals et des troquets, ils participèrent au tremplin « A travers Chant » (Saint-Saulve) où ils conquirent le jury, leur permettant d’atteindre la première place. S’enchainent alors un passage en studio pour l’enregistrement d’un EP 3 titres «Prologue» ainsi que la réalisation d’un clip vidéo sur leur titre «Garanti Sans Paraben».</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Toujours en quête de partage, ils décidèrent d’intégrer de nouveaux comédiens à la formation initiale. Ces personnages, partageant les mêmes ambitions, Les compositions émergent et se succèdent, donnant naissance à un second spectacle en 2019 jonglant entre comédie, swing, chanson française, hip-hop et amour.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Le groupe conquiert à nouveau le jury en 2020, celui de « Y’a pas l’feu » cette fois-ci,  lui permettant de remporter un nouveau tremplin musical.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Son premier album 10 titres « partisan du moindre effort » voit le jour cette même année ainsi que plusieurs vidéo-clips.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Le groupe se voit malheureusement annuler la totalité de ses dates pour les raisons sanitaires que nous connaissons bien.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Les Plastic Duck décident alors durant cette période très  "particulière" de continuer à promouvoir la musique par le biais des réseaux sociaux en proposant régulièrement des adaptations de leurs morceaux et  en mettant à contribution leur public.</Text>
+                    <Text></Text>
+                    <Text style={styles.bioText}>Quand les représentations ont enfin pu reprendre, c’est avec d’autant plus de conviction et de motivation que le groupe se prend au jeu, proposant un spectacle vivant, toujours plus dynamique et souriant.</Text>
+                </View>
             </ScrollView>
             <Footer />
         </SafeAreaView>
@@ -84,12 +125,34 @@ const styles = StyleSheet.create({
         fontSize: width * 0.035,
         marginBottom: width * 0.02,
     },
-    formationRowBoldText: {
-        fontFamily: 'Agency FB Bold'
+    formationContainer: {
+        backgroundColor: '#2C2C2C',
+        paddingHorizontal: width * 0.02,
+        paddingVertical: width * 0.01
+    },
+    formationRowNameText: {
+        fontFamily: 'Agency FB Bold',
+        color: 'white',
+        fontSize: width * 0.035
+    },
+    formationRowPracticeText: {
+        fontFamily: 'Agency FB',
+        color: 'white',
+        fontSize: width * 0.035
     },
     formationTitle: {
         fontFamily: 'Agency FB Bold',
         fontSize: width * 0.05,
-        color: 'white'
+        color: 'white',
+        marginBottom: width * 0.03
+    },
+    bioContainer: {
+        paddingHorizontal: width * 0.02,
+        paddingVertical: width * 0.01
+    },
+    bioText: {
+        fontFamily: 'Agency FB',
+        color: 'white',
+        fontSize: width * 0.03
     }
 })
